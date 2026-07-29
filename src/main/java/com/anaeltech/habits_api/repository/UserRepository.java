ffee1bbbf8c6846b31bddf81fb82e45b390f9295
AnaelTech <142304772+AnaelTech@users.noreply.github.com>
2026-07-29 10:09:17 +0200

    feat(userRepository): add method `getUserByEmail` and CRUD with JPArepository

---
+++ src/main/java/com/anaeltech/habits_api/repository/UserRepository.java
@@ -0,0 +1,16 @@
+package com.anaeltech.habits_api.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.anaeltech.habits_api.entity.User;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    // Optional is used to handle the case where a user with the given email may not
+    // exist in the database. It allows for a more expressive way to handle the
+    // absence of a value, avoiding potential NullPointerExceptions.
+    Optional<User> getUserByEmail(String email);
+
+}
