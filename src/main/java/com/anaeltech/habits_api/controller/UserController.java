2945b12e062b89ecf0508dba65f83b16d7586355
AnaelTech <142304772+AnaelTech@users.noreply.github.com>
2026-07-30 15:00:54 +0200

    ✨ (UserController.java): add endpoints to create and delete users

--- src/main/java/com/anaeltech/habits_api/controller/UserController.java
+++ src/main/java/com/anaeltech/habits_api/controller/UserController.java
@@ -1,16 +1,23 @@
 package com.anaeltech.habits_api.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.anaeltech.habits_api.dto.user.request.CreateUserRequest;
 import com.anaeltech.habits_api.dto.user.response.UserResponse;
 import com.anaeltech.habits_api.service.user.UserService;
 
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @RestController
 @RequestMapping("/api/${apiVersion}/users")
@@ -31,6 +38,28 @@ public class UserController {
     @GetMapping("/{id}")
     public UserResponse getUserById(@PathVariable Long id) {
         return userService.getUserById(id);
+    }
+
+    @Operation(summary = "Create a new user", description = "Create a new user with the provided details.", responses = {
+            @ApiResponse(responseCode = "201", description = "User created successfully"),
+            @ApiResponse(responseCode = "400", description = "Invalid input data"),
+            @ApiResponse(responseCode = "409", description = "User with the given email already exists"),
+            @ApiResponse(responseCode = "500", description = "Internal server error")
+    })
+    @PostMapping("/")
+    public ResponseEntity<UserResponse> createUser(@RequestBody CreateUserRequest userRequest) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(userService.createUser(userRequest));
+    }
+
+    @Operation(summary = "Delete user by ID", description = "Delete a user by their unique ID.", responses = {
+            @ApiResponse(responseCode = "204", description = "User deleted successfully"),
+            @ApiResponse(responseCode = "404", description = "User not found"),
+            @ApiResponse(responseCode = "500", description = "Internal server error")
+    })
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteUser(@PathVariable Long id) {
+        userService.deleteUser(id);
+        return ResponseEntity.noContent().build();
     }
 
 }
