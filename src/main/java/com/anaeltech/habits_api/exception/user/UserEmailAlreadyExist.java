5088285fd4de3da164a88b9c5d0f9d1f75a81e5c
AnaelTech <142304772+AnaelTech@users.noreply.github.com>
2026-07-30 14:44:45 +0200

    🔧 (user/UserEmailAlreadyExist.java): rename exception file

--- src/main/java/com/anaeltech/habits_api/exception/user/UserEmailAlreadyExist.java
+++
@@ -1,9 +0,0 @@
-package com.anaeltech.habits_api.exception.user;
-
-public class UserEmailAlreadyExist extends RuntimeException {
-
-    public UserEmailAlreadyExist(String email) {
-        super("User with email " + email + " already exists");
-    }
-
-}
--- src/main/java/com/anaeltech/habits_api/exception/user/UserPasswordDoesNotMatch.java
+++
@@ -1,9 +0,0 @@
-package com.anaeltech.habits_api.exception.user;
-
-public class UserPasswordDoesNotMatch extends RuntimeException {
-
-    public UserPasswordDoesNotMatch() {
-        super("User password does not match");
-    }
-
-}
